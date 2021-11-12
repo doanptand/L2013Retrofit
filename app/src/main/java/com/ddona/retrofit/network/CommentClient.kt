5745f9959fc759e81c99db92f968d7950225b152
DoanPT1 <doanptand@gmail.com>
2021-11-12 21:03:48 +0700

    add header

--- app/src/main/java/com/ddona/retrofit/network/CommentClient.kt
+++ app/src/main/java/com/ddona/retrofit/network/CommentClient.kt
@@ -1,6 +1,8 @@
 package com.ddona.retrofit.network
 
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,8 +11,23 @@ object CommentClient {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
 
+    val header = Interceptor {
+        val originRequest = it.request()
+
+        val newRequest = originRequest.newBuilder()
+            .header("Content-Type", "image/jpg")
+            .header("User-Agent", "Chrome...")
+            .build()
+        it.proceed(newRequest)
+    }
+
+    val okHttp = OkHttpClient.Builder()
+        .addInterceptor(header)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttp)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .build()
